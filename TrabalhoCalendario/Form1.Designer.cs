72ef5657da7190c71d099660c2d1ae7450f33b6d
Guilherme Ribeiro Paitax <guilhermepaitax@engefoto.com.br>
2019-05-15 15:04:37 -0300

    Add project files.

---
+++ TrabalhoCalendario/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TrabalhoCalendario
+{
+    partial class FormFeriados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.labelAno = new System.Windows.Forms.Label();
+            this.maskedTextBoxAno = new System.Windows.Forms.MaskedTextBox();
+            this.buttonCalcaular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(15, 105);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(510, 394);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // labelAno
+            // 
+            this.labelAno.AutoSize = true;
+            this.labelAno.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAno.ForeColor = System.Drawing.Color.White;
+            this.labelAno.Location = new System.Drawing.Point(18, 15);
+            this.labelAno.Name = "labelAno";
+            this.labelAno.Size = new System.Drawing.Size(97, 41);
+            this.labelAno.TabIndex = 1;
+            this.labelAno.Text = "Ano:";
+            // 
+            // maskedTextBoxAno
+            // 
+            this.maskedTextBoxAno.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBoxAno.Location = new System.Drawing.Point(121, 23);
+            this.maskedTextBoxAno.Mask = "0000";
+            this.maskedTextBoxAno.Name = "maskedTextBoxAno";
+            this.maskedTextBoxAno.Size = new System.Drawing.Size(153, 33);
+            this.maskedTextBoxAno.TabIndex = 2;
+            this.maskedTextBoxAno.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.maskedTextBoxAno.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            this.maskedTextBoxAno.ValidatingType = typeof(int);
+            // 
+            // buttonCalcaular
+            // 
+            this.buttonCalcaular.AutoSize = true;
+            this.buttonCalcaular.BackColor = System.Drawing.Color.White;
+            this.buttonCalcaular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonCalcaular.Location = new System.Drawing.Point(281, 23);
+            this.buttonCalcaular.Name = "buttonCalcaular";
+            this.buttonCalcaular.Size = new System.Drawing.Size(107, 33);
+            this.buttonCalcaular.TabIndex = 3;
+            this.buttonCalcaular.Text = "Calcular Feriados";
+            this.buttonCalcaular.UseVisualStyleBackColor = false;
+            this.buttonCalcaular.Click += new System.EventHandler(this.buttonCalcaular_Click);
+            // 
+            // FormFeriados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(540, 514);
+            this.Controls.Add(this.buttonCalcaular);
+            this.Controls.Add(this.maskedTextBoxAno);
+            this.Controls.Add(this.labelAno);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "FormFeriados";
+            this.Padding = new System.Windows.Forms.Padding(15);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Feriados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label labelAno;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxAno;
+        private System.Windows.Forms.Button buttonCalcaular;
+    }
+}
+
---
+++ TrabalhoCalendario/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhoCalendario
+{
+    public partial class FormFeriados : Form
+    {
+        public FormFeriados()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalcaular_Click(object sender, EventArgs e)
+        {
+            //Pegar o texto do maskedTextBoxAno e colocar na variavel ano convertentdo para int
+            int ano = Convert.ToInt32(maskedTextBoxAno.Text);
+            //Verifica se o ano digitado e acima de 1587
+            if (ano < 1587)
+            {
+                MessageBox.Show("O ano minimo permitido é 1587!", "Erro");//Exibe a mensagem de erro
+                return;//retorna pra não executar o resto
+            }
+
+            DateTime pascoa = Pascoa(ano);//Cria uma variavel do tipo Datetime que recebe a data da pascoa retornada pela funcão Pascoa()
+            richTextBox1.Clear();//Limpa o richTextBox1
+            richTextBox1.AppendText("Lista de Feriados de " + ano.ToString());//Mostra o texto Lista de Feriados junto ccom o ano digitado
+            richTextBox1.AppendText(Environment.NewLine);
+            richTextBox1.AppendText(Environment.NewLine);
+            richTextBox1.AppendText("Confraternizacao Universal: " + DiaDaSemana(ano, 01, 01) + ", " //Exibe o nome do feriado, em seguida o dia da semana retornado pelo metodo DiaDaSemana
+                + new DateTime(ano, 01, 01).ToShortDateString() + Environment.NewLine);//Exibe a data do feriado como o DateTime
+
+            int carnavalOrdinal = DataParaOrdinal(pascoa.Year, pascoa.Month, pascoa.Day) - 47;//Criando a variavel que recebe o dia ordinal retornado pela DataParaOrdinal da pascoa - 47
+            DateTime Carnaval = OrdinalParaData(carnavalOrdinal, ano);//Cria variavel que recebe a data apartir do dia ordinal
+            richTextBox1.AppendText("Carnaval: " + DiaDaSemana(Carnaval.Year, Carnaval.Month, Carnaval.Day) +
+                ", " + Carnaval.ToShortDateString() + Environment.NewLine);//Exibe as informacoes do feriado
+
+            int sextaSantaOrdinal = DataParaOrdinal(pascoa.Year, pascoa.Month, pascoa.Day) - 2;
+            DateTime sextaSanta = OrdinalParaData(sextaSantaOrdinal, ano);
+            richTextBox1.AppendText("Paixão de Cristo: " + DiaDaSemana(sextaSanta.Year, sextaSanta.Month, sextaSanta.Day) + 
+                ", " + sextaSanta.ToShortDateString() + Environment.NewLine);
+
+            richTextBox1.AppendText("Pascoa: " + DiaDaSemana(ano, pascoa.Month, pascoa.Day) + ", "
+                + new DateTime(ano, pascoa.Month, pascoa.Day).ToShortDateString() + Environment.NewLine);
+
+            richTextBox1.AppendText("Tiradentes: " + DiaDaSemana(ano, 04, 21) + ", " +
+                new DateTime(ano, 04, 21).ToShortDateString() + Environment.NewLine);
+
+            richTextBox1.AppendText("Dia do Trabalhador: " + DiaDaSemana(ano, 05, 01) + ", " +
+                new DateTime(ano, 05, 01).ToShortDateString() + Environment.NewLine);
+
+            int corpus = DataParaOrdinal(pascoa.Year, pascoa.Month, pascoa.Day) + 60;
+            DateTime corpusChristi = OrdinalParaData(corpus, ano);
+            richTextBox1.AppendText("Corpus Christi: " + DiaDaSemana(corpusChristi.Year, corpusChristi.Month, corpusChristi.Day) +
+                ", " + corpusChristi.ToShortDateString() + Environment.NewLine);
+
+            richTextBox1.AppendText("Dia da Pátria: " + DiaDaSemana(ano, 09, 07) + ", " +
+                new DateTime(ano, 09, 07).ToShortDateString() + Environment.NewLine);
+
+            richTextBox1.AppendText("Nossa Senhora Aparecida: " + DiaDaSemana(ano, 10, 12) + ", " +
+                new DateTime(ano, 10, 12).ToShortDateString() + Environment.NewLine);
+
+            richTextBox1.AppendText("Finados: " + DiaDaSemana(ano, 11, 02) + ", " +
+                new DateTime(ano, 11, 02).ToShortDateString() + Environment.NewLine);
+
+            richTextBox1.AppendText("Proclamação da República: " + DiaDaSemana(ano, 11, 15) + ", " +
+                new DateTime(ano, 11, 15).ToShortDateString() + Environment.NewLine);
+
+            richTextBox1.AppendText("Natal: " + DiaDaSemana(ano, 12, 25) + ", " +
+                new DateTime(ano, 12, 25).ToShortDateString() + Environment.NewLine);
+
+        }
+
+        private string DiaDaSemana(double ano, double mes, double dia)
+        {
+            //Criando array de strings dos dias da semana
+            string[] Dias = new string[] { "Sábado", "Domindo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira",
+                "Quinta-Feira", "Sexta-Feira" };
+            //Algoritimo para calcular o dia da semana
+            double a, b, c, d, e, f, g, h, i, j;
+            a = (12 - mes) / 10;
+            a = Math.Floor(a);
+            b = ano - a;
+            b = Math.Floor(b);
+            c = mes + (12 * a);
+            c = Math.Floor(c);
+            d = b / 100;
+            d = Math.Floor(d);
+            e = d / 4;
+            e = Math.Floor(e);
+            f = 2 - d + e;
+            f = Math.Floor(f);
+            g = 365.25 * b;
+            g = Math.Floor(g);
+            h = 30.6001 * (c + 1);
+            h = Math.Floor(h);
+            i = (f + g) + (h + dia) + 5;
+            i = Math.Floor(i);
+            j = i % 7;
+
+            return Dias[Convert.ToInt32(j)];//retorna o array Dias na posicao calculada pelo algoritimo representado por j
+        }
+
+        private DateTime Pascoa(double ano)
+        {
+            //Algoritimo para calcular a data da pascoa
+            int a, b, c, d, e, f, g, h, i, k, l, m, an = Convert.ToInt32(ano), mes, dia;
+            a = (an % 19);
+            b = an / 100;
+            c = (an % 100);
+            d = b / 4;
+            e = (b % 4);
+            f = (b + 8) / 25;
+            g = (b - f + 1) / 3;
+            h = ((19 * a + b - d - g + 15) % 30);
+            i = c / 4;
+            k = (c % 4);
+            l = ((32 + 2 * e + 2 * i - h - k) % 7);
+            m = (a + 11 * h + 22 * l) / 451;
+            mes = (h + l - 7 * m + 114) / 31;
+            dia = ((h + l - 7 * m + 114) % 31) + 1;
+            return (new DateTime(an, mes, dia));//retorna um DateTime do dia mes e ano da pascoa
+        }
+
+        
+        private Boolean Bissexto(int ano)
+        {
+            //verifica se o ano recebido por parametro tem resto 0 na divisão por 4 e tem resto diferente de 0 na divisão por 100
+            if (ano % 4 == 0 && ano % 100 != 0)
+            {
+                //Retorna true ou seja e um ano bissexto
+                return true;
+            }
+            else
+            {
+                //retorna false ou seja não e um ano bissexto
+                return false;
+            } 
+        } 
+
+        private int[] GeraTabelaMeses(int ano)
+        {
+            int[] mes = new int[] {0,31,28,31,30,31,30,31,31,30,31,30,31};//Cria um array de int com a quantia de dias dos meses
+
+            if (Bissexto(ano) == false)//Verifia se o ano recebido e bissexto
+            {
+                return mes;//Caso não seja apenas retorna o array dos meses
+            }
+            else
+            {
+                mes[2] = 29;//Caso seja bissexto a posicão 2 do array recebe 29 dias e retorna o array
+                return mes;
+            }     
+        }
+
+
+        private int[] GeraTabelaOrdinais(int ano)
+        {
+            int[] mes = GeraTabelaMeses(ano);//array recebe retorno da funcão GeraTabelaMeses
+            int[] ordinais = new int[12];//Criando array de ordinais
+            ordinais[0] = 0;
+            for (int i = 1; i < 12; i++)//Fazendo um for para percorrer o array dos meses
+            {  
+                ordinais[i] = mes[i] + ordinais[i - 1];//ordinais recebe a quantida de dias do mes somado com a quantia de ordinais da pocicao anterior  
+            }
+            return ordinais;//retorna o array de ordinais
+        }
+
+        private int DataParaOrdinal(int ano, int mes, int dia)
+        {
+            int[] ordinais = GeraTabelaOrdinais(ano);//recebe o array de ordinais da GeraTabelaOrdinais
+            return (ordinais[mes - 1] + dia);//retorna a data ordinal que se da pelo dia somado pelo array de ordinais na posicao mes menos 1
+        }
+
+        private DateTime OrdinalParaData(int diaOrdinal, int ano)
+        {
+            int mes = 0, dia = 0;
+            int[] ordinais = GeraTabelaOrdinais(ano);
+            for (int i = 0; i < 12; i++)//For para percorrer o array de ordinais
+            {
+                if (diaOrdinal > ordinais[i])//verifica de o diaOrdinal e maior que o ordinasi na posicao i
+                {
+                    mes = i + 1;//variavel mes recebe o valor do i mais 1
+                    dia = diaOrdinal - ordinais[i];//variavel dia recebe o diaOrdinal subtraido pelo ordinais na posicao i
+
+                }
+            }
+            return (new DateTime(ano, mes, dia));//retorna um DateTime da data do dia em ordinal 
+        }  
+    }
+}
